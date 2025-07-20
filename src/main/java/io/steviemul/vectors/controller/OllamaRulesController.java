1a1deb382a7206ae716967d2dbb208207a0d5ea3
Stephen Mulrennan <stephen.mulrennan@gmail.com>
2025-07-20 10:53:48 +0100

    Add some changes.

--- src/main/java/io/steviemul/vectors/controller/OllamaRulesController.java
+++ src/main/java/io/steviemul/vectors/controller/OllamaRulesController.java
@@ -5,6 +5,9 @@ import io.steviemul.vectors.entity.QueryAdjustments;
 import io.steviemul.vectors.entity.RuleRequest;
 import io.steviemul.vectors.service.RulesVectorService;
 import java.util.List;
+import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class OllamaRulesController {
 
   public static final String RULES_URI = "/ollama/rules";
+  public static final String RULE_IDS_URL = RULES_URI + "/ids";
+  public static final String ID = "id";
 
   private final RulesVectorService rulesVectorService;
 
-  public OllamaRulesController(@Qualifier("ollamaRulesVectorService") RulesVectorService rulesVectorService) {
+  public OllamaRulesController(
+      @Qualifier("ollamaRulesVectorService") RulesVectorService rulesVectorService) {
     this.rulesVectorService = rulesVectorService;
   }
 
@@ -42,5 +48,19 @@ public class OllamaRulesController {
       @PathVariable("vendor") final String vendor,
       QueryAdjustments adjustments, RuleRequest ruleRequest) {
     return rulesVectorService.search(vendor, adjustments, ruleRequest);
+  }
+
+  @GetMapping(RULE_IDS_URL + "/{vendor}")
+  public Map<Object, Double> getSimilarRulesForVendor(
+      @PathVariable("vendor") final String vendor,
+      QueryAdjustments adjustments, RuleRequest ruleRequest) {
+
+    return rulesVectorService.search(vendor, adjustments, ruleRequest)
+        .stream().collect(
+            Collectors.toMap(
+                d -> d.metadata().get(ID),
+                DocumentResponse::score,
+                (d1, d2) -> d1,
+                LinkedHashMap::new));
   }
 }
